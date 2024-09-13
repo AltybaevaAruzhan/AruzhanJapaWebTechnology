7fa17b95739aed37c3a24f480201e5fd28ed2249
Aruzhan Altybaeva <aruzhan.altybaeva15@gmail.com>
2024-09-13 12:33:00 +0600

    first app

---
+++ src/main/java/com/example/AruzhanJapaWebTechnology/AruzhanJapaWebTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.AruzhanJapaWebTechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AruzhanJapaWebTechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AruzhanJapaWebTechnologyApplication.class, args);
+	}
+
+}
